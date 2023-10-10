12e8e47f7dd4bce0e70b7da7861fd3f21a23ac2a
Gabomou07 <gabriel.vegabalcazar@gm,ail.com>
2023-10-10 18:12:53 -0500

    Login "Carcasa + funcionalidad" en proceso

---
+++ proyectov1/LoginTableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginTableViewController.swift
+//  proyectov1
+//
+//  Created by DAMII on 10/10/23.
+//
+
+import UIKit
+
+class LoginTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
