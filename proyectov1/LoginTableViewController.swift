b13a343443ea98efe267bace119a2d8d97bc8015
Gabomou07 <gabriel.vegabalcazar@gm,ail.com>
2023-10-12 17:23:12 -0500

    Login = Frontend acabado - Gabriel Vega

--- proyectov1/LoginTableViewController.swift
+++ proyectov1/LoginTableViewController.swift
@@ -13,5 +13,19 @@ class LoginTableViewController: UITableViewController {
         super.viewDidLoad()
 
     }
-
+    
+//    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return UIScreen.main.bounds.height
+//    }
+    //Posicionamiento del Login
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let tableViewHeight = self.tableView.frame.height
+        let contentHeight = self.tableView.contentSize.height
+        
+        let centeringInset = (tableViewHeight - contentHeight)/2.0
+        let topInset = max(centeringInset, 0.0)
+        
+        self.tableView.contentInset = UIEdgeInsets(top: topInset, left: 0.0, bottom: 0.0, right: 0.0)
+    }
 }
